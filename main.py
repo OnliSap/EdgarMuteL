e9eb16842b56ccbf61d36d06690c6b333fad4aa5
tishaker <tishakov.ilya.sergeevich@gmail.com>
2026-05-05 06:42:22 +0200

    Теперь 2 темы и меняются они сразу и запоминаются, ini файл работает, всё оформление в функции теперь

--- main.py
+++ main.py
@@ -16,80 +16,49 @@ class MainWindow(QtWidgets.QWidget):
         self.theme_index = int(config['user']['theme_index'])
         self.style_type = themes[self.theme_index]
         
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.setContentsMargins(35, 25, 35, 25)
+        self.layout.setSpacing(10)
         
-        self.setStyleSheet(f"QWidget {{ background-color: {config[self.style_type]['background']}; }}")
-
-        layout = QtWidgets.QVBoxLayout(self)
-        layout.setContentsMargins(35, 25, 35, 25)
-        layout.setSpacing(10)
-        
-        # ПОЛНОСТЬЮ ТЕМНЫЙ СТИЛЬ ДЛЯ СПИСКОВ
-        dark_combo_style = f"""
-            QComboBox {{ 
-                {config[self.style_type]['qcombobox']} 
-            }}
-            QComboBox::drop-down {{ 
-                border: 0px; 
-            }}
-            QComboBox::down-arrow {{ 
-                {config[self.style_type]['qcombobox_downarrow']} 
-            }}
-            QComboBox QAbstractItemView {{
-                {config[self.style_type]['qcombobox_menu']}
-            }}
-        """
-
+        # Create UI elements
         self.header = QtWidgets.QLabel("STATUS: SYNCING...")
-        self.header.setStyleSheet(f"color: {config[self.style_type]['header_color']}; font-weight: bold; font-size: 14px; margin-bottom: 10px;")
         self.header.setAlignment(QtCore.Qt.AlignCenter)
-        layout.addWidget(self.header)
+        self.layout.addWidget(self.header)
 
-        def create_label(text):
-            lbl = QtWidgets.QLabel(text)
-            lbl.setStyleSheet(f"color: {config[self.style_type]['label_color']}; font-weight: bold; font-size: 12px; margin-top: 5px;")
-            return lbl
-
-        layout.addWidget(create_label("ВЫБЕРИТЕ ЖЕРТВУ:"))
         self.user_box = QtWidgets.QComboBox()
-        self.user_box.setStyleSheet(dark_combo_style)
-        layout.addWidget(self.user_box)
+        self.layout.addWidget(self.user_box)
 
-        layout.addWidget(create_label("ТИП НАКАЗАНИЯ:"))
         self.action_box = QtWidgets.QComboBox()
         self.action_box.addItems(["Тайм-аут", "Откл. микрофон", "Откл. звук", "КИКНУТЬ", "ЗАБАНИТЬ"])
-        self.action_box.setStyleSheet(dark_combo_style)
-        layout.addWidget(self.action_box)
+        self.layout.addWidget(self.action_box)
 
-        layout.addWidget(create_label("ВРЕМЯ:"))
         self.time_box = QtWidgets.QComboBox()
         self.time_box.addItems(["30 сек", "1 мин", "5 мин", "30 мин", "1 час", "24 часа", "Навсегда"])
-        self.time_box.setStyleSheet(dark_combo_style)
-        layout.addWidget(self.time_box)
+        self.layout.addWidget(self.time_box)
 
-        layout.addWidget(create_label("ПРИЧИНА:"))
         self.reason_input = QtWidgets.QLineEdit()
         self.reason_input.setPlaceholderText("За что наказываем?")
-        self.reason_input.setStyleSheet(f"QLineEdit {{ {config[self.style_type]['qlineedit']} }}")
-        layout.addWidget(self.reason_input)
+        self.layout.addWidget(self.reason_input)
 
-        layout.addStretch()
+        self.layout.addStretch()
 
         self.btn_sync = DecoButton("ОБНОВИТЬ СПИСОК", "#10b981", fsize=12)
         self.btn_exec = DecoButton("ИСПОЛНИТЬ ПРИГОВОР", "#ef4444")
         self.btn_exec.setFixedSize(380, 60)
-        
-        layout.addWidget(self.btn_sync, alignment=QtCore.Qt.AlignCenter)
-        layout.addWidget(self.btn_exec, alignment=QtCore.Qt.AlignCenter)
+        self.layout.addWidget(self.btn_sync, alignment=QtCore.Qt.AlignCenter)
+        self.layout.addWidget(self.btn_exec, alignment=QtCore.Qt.AlignCenter)
 
         self.btn_sync.clicked.connect(self.fetch_users)
         self.btn_exec.clicked.connect(self.send_command)
 
-        self.user_data = {}
-        QtCore.QTimer.singleShot(1000, self.fetch_users)
-        
-        theme_btn = DecoButton("СМЕНИТЬ ТЕМУ", "#64748b", fsize=8)
+        theme_btn = DecoButton("СМЕНИТЬ ТЕМУ", "#64748b", fsize=10)
+        theme_btn.setFixedSize(190, 30)
         theme_btn.clicked.connect(self.change_theme_written)
-        layout.addWidget(theme_btn, alignment=QtCore.Qt.AlignRight)
+        self.layout.addWidget(theme_btn, alignment=QtCore.Qt.AlignRight)
+        
+        self.user_data = {}
+        self.set_styles()
+        QtCore.QTimer.singleShot(1000, self.fetch_users)
         
         
     def fetch_users(self):
@@ -103,6 +72,7 @@ class MainWindow(QtWidgets.QWidget):
         except:
             self.header.setText("STATUS: BOT OFFLINE")
             self.header.setStyleSheet("color: #ef4444; font-weight: bold;")
+
 
     def send_command(self):
         user_name = self.user_box.currentText()
@@ -120,11 +90,45 @@ class MainWindow(QtWidgets.QWidget):
         except:
             self.header.setText("SEND ERROR")
             
+    
+    def set_styles(self):
+        # Background
+        self.setStyleSheet(f"QWidget {{ background-color: {config[self.style_type]['background']}; }}")
+        
+        # ComboBox style
+        combo_style = f"""
+            QComboBox {{ 
+                {config[self.style_type]['qcombobox']} 
+            }}
+            QComboBox::drop-down {{ 
+                border: 0px; 
+            }}
+            QComboBox::down-arrow {{ 
+                {config[self.style_type]['qcombobox_downarrow']} 
+            }}
+            QComboBox QAbstractItemView {{
+                {config[self.style_type]['qcombobox_menu']}
+            }}
+        """
+        
+        self.header.setStyleSheet(f"color: {config[self.style_type]['header_color']}; font-weight: bold; font-size: 14px; margin-bottom: 10px;")
+        self.user_box.setStyleSheet(combo_style)
+        self.action_box.setStyleSheet(combo_style)
+        self.time_box.setStyleSheet(combo_style)
+        self.reason_input.setStyleSheet(f"QLineEdit {{ {config[self.style_type]['qlineedit']} }}")
+        
+        # Update labels in layout
+        label_style = f"color: {config[self.style_type]['label_color']}; font-weight: bold; font-size: 12px; margin-top: 5px;"
+        for i in range(self.layout.count()):
+            widget = self.layout.itemAt(i).widget()
+            if isinstance(widget, QtWidgets.QLabel) and widget != self.header:
+                widget.setStyleSheet(label_style)
             
     def change_theme_written(self):
-        self.theme_index = (self.theme_index + 1) % 2 # only black an white now
+        self.theme_index = (self.theme_index + 1) % 2
         self.style_type = themes[self.theme_index]
         theme_change(config, self.theme_index)
+        self.set_styles()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
